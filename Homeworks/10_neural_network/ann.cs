0ca751e7652d21c2c9497838287972218fbe8c59
Emma Rostal <emmarostal@gmail.com>
2023-07-04 14:56:30 +0200

    Part A done

---
+++ Homeworks/10_neural_network/ann.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Threading;
+using static System.Console;
+using static System.Math; 
+using System.IO;
+using System.Collections.Generic;
+public class ann{
+    public int n; /* number of hidden neurons */
+    public Func<double,double> f = x => x*Exp(-x*x); /* activation function */
+    public Func<double,double> firstD = x => (1-2*x*x)*Exp(-x*x); /* derivative of activation function */
+    public Func<double,double> secondD= x => Exp(-x*x)*(4*x*x*x-6*x); /* 2nd derivative of activation function */
+    public Func<double,double> antiD = x => -0.5*Exp(-x*x); /* first antiderivative of activation function */
+    public vector p; /* network parameters */
+    public int nsteps;
+    public ann(int n){
+        this.n = n; 
+        int numParams = 3*n; //n a_i, n b_i, n w's
+        p = new vector(numParams);
+    }
+    public double response(double x){
+        double result = 0;
+        for(int i = 0; i < n; i++){
+            double a_i = p[i];
+            double b_i = p[n+i];
+            double w_i = p[2*n+i];
+            result += w_i * f( (x-a_i)/b_i );
+        }
+        return result;
+    }
+    public double firstDResponse(double x){
+        /* return the first derivative of the response of the network to the input signal x */
+        double result = 0;
+        for(int i = 0; i<n; i++){
+            double ai = p[i];
+            double bi = p[i+n];
+            double wi = p[i+2*n];
+            result += wi * firstD((x-ai)/bi)/bi;
+        }
+        return result;
+    }
+
+    public double secondDResponse(double x){
+        /* return the second derivative of the response of the network to the input signal x */
+        double result = 0;
+        for(int i = 0; i<n; i++){
+            double ai = p[i];
+            double bi = p[i+n];
+            double wi = p[i+2*n];
+            result += wi * secondD((x-ai)/bi)/(bi*bi);
+        }
+        return result;
+    }
+    public double antiDRespone(double x, double a){
+        /* return the second derivative of the response of the network to the input signal x */
+        double result = 0;
+        for(int i = 0; i<n; i++){
+            double ai = p[i];
+            double bi = p[i+n];
+            double wi = p[i+2*n];
+            result += wi * bi * (antiD((x-ai)/bi) - antiD((a-ai)/bi));
+        }
+        return result;
+    }
+   public void train(vector x,vector y){
+        /* train the network to interpolate the given table {x,y} */
+        /*initialize parameter vector randomly*/
+        var rand = new Random();
+        for (int i = 0; i < p.size; i++){
+            p[i] = rand.NextDouble() - 0.5;
+        }
+        Func<vector,double> c = ps => {
+            double toMin = 0; 
+            for(int j = 0; j < x.size; j++){
+                double result = 0; 
+                for(int i = 0; i < n; i++){
+                    double a_i = ps[i];
+                    double b_i = ps[n+i];
+                    double w_i = ps[2*n+i];
+                    result += w_i * f( (x[j]-a_i)/b_i );
+                }
+                toMin += Pow(result-y[j],2);
+            }
+            return toMin/n;
+        };
+        p = minimizer.qnewton(c,p,0.001).Item1;
+    
+   }
+}
--- Homeworks/10_neural_network/main.cs
+++ Homeworks/10_neural_network/main.cs
@@ -7,5 +7,51 @@ using System.Collections.Generic;
 
 public static class main{
     public static void Main(){
+        string border = new string('-',40);
+        WriteLine(border);
+        WriteLine("Part A");
+        WriteLine(border);
+
+        int dataSize = 20;
+		vector xs = new vector(dataSize);
+		vector ys = new vector(dataSize);
+
+        WriteLine("Making training points");
+		string toWrite = "";
+		for(int i = 0; i < dataSize; i++){
+			double x = 2.0 * i / dataSize - 1;
+			xs[i] = x;
+			ys[i] = Wavelet(x);
+			toWrite += $"{xs[i]}\t{ys[i]}\n";
+		}
+		File.WriteAllText("trainingPoints.txt",toWrite);
+
+        WriteLine("Interpolating using 5 neurons");
+		ann myNN = new ann(5);
+		myNN.train(xs,ys);
+
+		//create interpolation set
+		toWrite = "";
+		int interpolationPoints = 100;
+		for(int i = 0; i < interpolationPoints; i++){
+			double x = 2.0 * i / interpolationPoints - 1;
+			toWrite += $"{x}\t{myNN.response(x)}\n";
+		}
+		File.WriteAllText("5neuronInterp.txt",toWrite);
+		WriteLine("The result can be seen in Training.svg.");
+		WriteLine(border);
+		
     }
+
+    public static double Gaussian(double x){
+        return Exp(-Pow(x,2));
+    }
+    public static double GaussianW(double x){
+        return x*Exp(-Pow(x,2));
+    }
+    public static double Wavelet(double x){
+        return Cos(5*x)*Exp(-Pow(x,2)); 
+    }
+
+
 }
